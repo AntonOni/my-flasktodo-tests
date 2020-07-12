b190dc5879e4660dfe69261b37d7715987b71a15
lisetskywork@gmail.com <lisetskywork@gmail.com>
2020-07-12 15:56:02 -0700

    pr

--- test_ui/test_main/conftest.py
+++ test_ui/test_main/conftest.py
@@ -7,7 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def driver_setup(request):
     chrome = webdriver.Chrome(ChromeDriverManager().install())
     chrome.maximize_window()
-    chrome.get("http://ec2-54-167-34-122.compute-1.amazonaws.com:5000/")
+    chrome.get("http://0.0.0.0:5000/")
     chrome.implicitly_wait(10)
     request.cls.chrome = chrome
     yield
