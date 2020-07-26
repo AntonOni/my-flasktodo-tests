2d895d318643d4926e46fdb93545eb75e2f76de1
lisetskywork@gmail.com <lisetskywork@gmail.com>
2020-07-26 12:55:10 -0700

    pr

--- test_ui/test_main/conftest.py
+++ test_ui/test_main/conftest.py
@@ -6,10 +6,10 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="class")
 def driver_setup(request):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
-    # chrome = webdriver.Chrome(ChromeDriverManager().install())
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    # chrome = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    chrome = webdriver.Chrome(ChromeDriverManager().install())
     chrome.maximize_window()
     chrome.get("http://0.0.0.0:5000")
     chrome.implicitly_wait(10)
